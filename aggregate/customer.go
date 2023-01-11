be132a4e96b8e037b90bcec97abc21a82afe4434
Masayuki <soy2.san@gmail.com>
2023-01-12 01:21:32 +0900

    aggregates (combine entities)

---
+++ aggregate/customer.go
@@ -0,0 +1,48 @@
+package aggregate
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"github.com/soysan/go_ddd_practice/entity"
+	"github.com/soysan/go_ddd_practice/valueobject"
+)
+
+var ErrInvalidPerson = errors.New("a customer has to have an valid person")
+
+type Customer struct {
+	person       *entity.Person
+	products     []*entity.Item
+	transactions []valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
+
+func (c *Customer) GetID() uuid.UUID {
+	return c.person.ID
+}
+
+func (c *Customer) SetID(id uuid.UUID) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.ID = id
+}
+
+func (c *Customer) GetName() string {
+	return c.person.Name
+}
---
+++ aggregate/customer_test.go
@@ -0,0 +1,35 @@
+package aggregate_test
+
+import (
+	"github.com/soysan/go_ddd_practice/aggregate"
+	"testing"
+)
+
+func TestCustomer_NewCustomer(t *testing.T) {
+	type testCase struct {
+		test        string
+		name        string
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			test:        "Empty Name validation",
+			name:        "",
+			expectedErr: aggregate.ErrInvalidPerson,
+		}, {
+			test:        "Valid Name",
+			name:        "Percy Bolmer",
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			_, err := aggregate.NewCustomer(tc.name)
+			if err != tc.expectedErr {
+				t.Errorf("Expected error %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
